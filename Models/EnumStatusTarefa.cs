5ce48ef3d15abd2117d86a00217f9147747bd4dd
Marcos Aleixo <maknytz@gmail.com>
2025-10-22 19:09:28 -0300

    Criando o Enum de Status da Tarefa

---
+++ Models/EnumStatusTarefa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dio_dotnet_desafio_api.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
