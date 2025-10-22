53923a513f16b856aaf0f8379a9558c015e07479
Marcos Aleixo <maknytz@gmail.com>
2025-10-22 19:15:44 -0300

    Ajustando o programa principal

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using dio_dotnet_desafio_api.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<OrganizadorContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal")));
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
